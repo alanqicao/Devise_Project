b400091bac2428ee6d08d8dbf781b7a9e8f0cb56
Qi Cao <alanqicao@gmail.com>
2020-03-02 12:38:34 -0700

    update

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,52 @@
 class FoodsController < ApplicationController
+  before_action :set_food, only:[:show,:edit,:update]
+  
   def index
+    @foods = current_user.foods
   end
 
   def show
   end
 
   def new
+    @food = Food.new
+  end
+
+  def create
+    @food = current_user.foods.new(food_params)
+    if @food.save
+      flash[:success] = "food Created"
+    redirect_to foods_path
+    else
+    render :new
+    flash[:error] = "Error #{@food.errors.full_messages.join('\n')}"
+    end
   end
 
   def edit
   end
+
+  def update
+    if @food.update(food_params)
+      redirect_to foods_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @food.destroy
+    redirect_to foods_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :calorie)
+  end
+
+  def set_food
+    @food = current_user.foods.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root  "accounts#index"
+  get 'static_pages/index'
+  root  "static_pages#index"
+  
   devise_for :users
 
-  resources :accounts
+  resources :accounts 
+    resources :foods
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
